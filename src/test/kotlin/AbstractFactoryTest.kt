a63d9e4bc96fa497ff19fb8025f999f0aa134ed5
itsandreramon <thielea@th-brandenburg.de>
2024-07-20 00:06:05 +0200

    add test

--- src/test/kotlin/AbstractFactoryTest.kt
+++ src/test/kotlin/AbstractFactoryTest.kt
@@ -4,9 +4,16 @@ import org.junit.jupiter.api.Test
 class AbstractFactoryTest {
 
 	@Test
-	fun test() {
+	fun `test WotifWidgetFactory`() {
 		val factory: WidgetFactory = WidgetFactory.MotifWidgetFactory
 		assertEquals("MotifScrollBar", factory.createScrollBar().name)
 		assertEquals("MotifWindow", factory.createWindow().name)
+	}
+
+	@Test
+	fun `test PMWidgetFactory`() {
+		val factory: WidgetFactory = WidgetFactory.PMWidgetFactory
+		assertEquals("PMScrollBar", factory.createScrollBar().name)
+		assertEquals("PMWindow", factory.createWindow().name)
 	}
 }
